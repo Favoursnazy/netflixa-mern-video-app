d4a841463f56cff8f3e71e21969713bb794d3e74
Favoursnazy4u <favoursnazy4u@gmail.com>
2023-04-15 22:22:50 +0100

    comment

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const BASEURL = import.meta.env.VITE_API_URL;
 
 const Axios = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASEURL,
 });
 
 export default Axios;
